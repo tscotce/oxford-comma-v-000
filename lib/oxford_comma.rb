ad503c7094edda2492d6a4859c39e9db24e1238b
Tatyana Scotce <tscotce@gmail.com>
2018-02-12 22:44:09 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length < 2
+  array.join
+  elsif array.length == 2 
+  array.join(" and ")
+  elsif array.length >= 3
+  new_array = []
+  new_array << array[0...array.length-1].join(", ")
+  new_array << array[-1]
+  new_array.join(", and ")
+  end
 end
