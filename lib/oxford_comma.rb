b03fcd675c34733bd2c3f596a4165398452aa7cf
Tatyana Scotce <tscotce@gmail.com>
2018-02-12 22:15:42 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
-  if array.length == 2
-  array.join(", ")
+  if array.length < 2
+  array
+  elsif array.length == 2 
+  array.join(" and ")
   elsif array.length == 3
   array.join(", ")
   end
