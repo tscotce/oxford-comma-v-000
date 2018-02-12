6e8dbf5556745d5d72b5b1c7e91143ec599b4dbc
Tatyana Scotce <tscotce@gmail.com>
2018-02-12 22:42:52 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   array.join
   elsif array.length == 2 
   array.join(" and ")
-  elsif array.length > 3
+  elsif array.length => 3
   new_array = []
   new_array << array[0...array.length-1].join(", ")
   new_array << array[-1]
